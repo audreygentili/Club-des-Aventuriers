6aacf4c7b7919e62468f60660d1a9a56244ae14c
Audrey Gentili <audrey.gentili38210@gmail.com>
2022-12-28 16:54:37 +0100

    Update assets names

--- game.js
+++ game.js
@@ -1,15 +1,20 @@
 let audio, table, ctxT, plateau, ctxP, img1, img2, txt, btn;
 let lives = 3;
+let mission = 4;
 
+// fonction onload
 onload = function () {
-	audio = get("#audio1");
+	audio = get("#audio");
     if (audio != null) {
-        audio.onplay = fPlay;
+        if (mission <=5) audio.src = "assets/Aventure1/mission"+mission+".mp3";
+        else audio.src = "assets/Aventure1/epilogue.mp3";
+        audio.onended = fPlay;
     }
     btn = get("#game");
     if (btn != null) {
         btn.onclick = function() { window.location.href = "game.html"; };
     }
+    // table de jeu
     table = get("#table");
     ctxT = table.getContext('2d'); 
     img1 = new Image();
@@ -19,6 +24,8 @@ onload = function () {
         table.width = "800";
         ctxT.drawImage(img1, 0, 0);
     };
+
+    // plateau de jeu permettant de compter les points
     plateau = get("#plateau");
     ctxP= plateau.getContext('2d'); 
     img2= new Image();
@@ -27,27 +34,33 @@ onload = function () {
         plateau.height = img2.naturalHeight;
         plateau.width = img2.naturalWidth;
         ctxP.drawImage(img2, 0, 0);
-        if (lives >= 1) {
+        if (lives >= 1) { // si l'équipe a une vie ou plus
             ctxP.beginPath();
             ctxP.arc(45,185,30,0,2*Math.PI);
             ctxP.fillStyle = '#ECDA2A';
             ctxP.fill();
             ctxP.stroke();
         }
-        if (lives >= 2) {
+        if (lives >= 2) { // si l'équipe a deux vies ou plus
             ctxP.beginPath();
             ctxP.arc(55,270,30,0,2*Math.PI);
             ctxP.fillStyle = '#ECDA2A';
             ctxP.fill();
             ctxP.stroke();
         }
-        if (lives >= 3) {
+        if (lives = 3) { // si l'équipe a trois vies
             ctxP.beginPath();
             ctxP.arc(110,335,30,0,2*Math.PI);
             ctxP.fillStyle = '#ECDA2A';
             ctxP.fill();
             ctxP.stroke();
         }
+        ctxP.beginPath();
+        let ht = 485-mission*65;
+        ctxP.arc(250,ht,15,0,2*Math.PI);
+        ctxP.fillStyle = 'black';
+        ctxP.fill();
+        ctxP.stroke();
     };
 }
 
@@ -57,5 +70,5 @@ function get(sel) {
 
 function fPlay() {
     txt = get("#p1");
-	txt.innerHTML = "Mission 1";
+	txt.innerHTML = "Mission "+mission+" : ";
 }
